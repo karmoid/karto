2992853a28f687bb78e11b00cf7ac0719e59094e
Marc Chauffour <karmoc@gmail.com>
2013-01-06 12:06:39 +0100

    Commit de debut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karto::Application.config.secret_token = 'e0972c873196f2aea23c51e5a0654c8f2528b1fd5260fd5415c05993a23ee562fc0fd2b07a413796d49781cd84cdd4ff62ea7f1ff6fa1daac80f70105ef77a81'
