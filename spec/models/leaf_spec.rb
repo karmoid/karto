308ae987f7c20bf1c9ed7d813b5d25e39a9cee0c
Marc Chauffour <karmoc@gmail.com>
2013-01-17 07:54:26 +0100

    ajout rep et cie

--- spec/models/leaf_spec.rb
+++ spec/models/leaf_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Leaf do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before(:each) do
+		@leaf = Leaf.new(:name => "leaf1", :hr => "leaf lib1")
+	end
+
+	it "is valid with valid attributes" do
+		@leaf.should be_valid
+	end	
+
+	it "is not valid without a name" do
+		@leaf.name = nil
+		@leaf.should_not be_valid
+	end	
+
+	it "is not valid without a hr" do
+		@leaf.hr = nil
+		@leaf.should_not be_valid
+	end	
+
 end
